1c676a02374be5428215cc63df42f9466accfc2f
OleksandrNechai <nechais@gmail.com>
2015-09-28 15:44:49 +0300

    Initial commit

---
+++ CSharp6Demo/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+
+        public static void CheckForNull<T>(T val, string name) where T : class
+        {
+            if (val == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+
+    class Circle
+    {
+        public Point Centre
+        {
+            get { return _centre; }
+        }
+
+        private readonly Point _centre = new Point(0, 0);
+
+        public double Radius
+        {
+            get { return _radius; }
+        }
+
+        private readonly double _radius = 1.0;
+
+        public Circle()
+        {
+        }
+
+        public Circle(Point centre, double radius)
+        {
+            Program.CheckForNull(centre, "centre");
+            this._centre = centre;
+            this._radius = radius;
+        }
+
+        public Circle(string input)
+        {
+            var array = input.Split(',');
+            double x, y, radius;
+            double.TryParse(array[0], out x);
+            double.TryParse(array[0], out y);
+            double.TryParse(array[0], out radius);
+            _centre = new Point(x, y);
+            _radius = radius;
+        }
+
+        public static Circle FromDatabaseData(Point centre, double radius)
+        {
+            double x = 0, y = 0;
+            if (centre != null)
+            {
+                x = centre.X;
+                y = centre.Y;
+            }
+            return new Circle(new Point(x, y), radius);
+        }
+
+        public void Print(Action<string> printer)
+        {
+            printer(String.Format("Centre coordinates: {0}, Radius: {1}", _centre, _radius));
+        }
+
+        public double Area
+        {
+            get { return Math.PI * Math.Pow(this.Radius, 2); }
+        }
+
+        public bool Intersects(Circle otherCircle)
+        {
+            return
+                Math.Sqrt(
+                    Math.Pow((this.Centre.X - otherCircle.Centre.X), 2) +
+                    Math.Pow((this.Centre.Y - otherCircle.Centre.Y), 2)) <= (this.Radius + otherCircle.Radius);
+        }
+    }
+
+    class Point
+    {
+        private readonly double _x;
+        private readonly double _y;
+
+        public Point(double x, double y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", _x, _y);
+        }
+    }
+}
---
+++ CSharp6Demo/Tests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSharp6Demo
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void InitCircleWithNullRadiusThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Circle(null, 0));
+        }
+        [Test]
+        public void CanConsttructDefaultCircuit()
+        {
+            var text = "";
+            Action<string> printer = (input) => text = input;
+            new Circle().Print(printer);
+            Assert.That(text, Is.EqualTo("Centre coordinates: (0,0), Radius: 1"));
+        }
+        [Test]
+        public void CanConsttructValidCircuit()
+        {
+            var text = "";
+            Action<string> printer = (input) => text = input;
+            new Circle(new Point(1,1), 1).Print(printer);
+            Assert.That(text, Is.EqualTo("Centre coordinates: (1,1), Radius: 1"));
+        }
+        [Test]
+        public void CanConsttructFromDatabaseWheninputNull()
+        {
+            var text = "";
+            Action<string> printer = (input) => text = input;
+            Circle.FromDatabaseData(null,1).Print(printer);
+            Assert.That(text, Is.EqualTo("Centre coordinates: (0,0), Radius: 1"));
+        }
+        [Test]
+        public void CanConsttructFromDatabaseWheninputNotNull()
+        {
+            var text = "";
+            Action<string> printer = (input) => text = input;
+            Circle.FromDatabaseData(new Point(1,1), 1).Print(printer);
+            Assert.That(text, Is.EqualTo("Centre coordinates: (1,1), Radius: 1"));
+        }
+        [Test]
+        public void MovingCircleToDefinedPoint()
+        {
+            var text = "";
+            Action<string> printer = (input) => text = input;
+            var c1 = new Circle(new Point(0, 0), 2);
+            var c2 = new Circle(new Point(1, 1), 2);
+
+            Assert.IsTrue(c1.Intersects(c2));
+        }
+        [Test]
+        public void AreaCalculation()
+        {
+            var c = new Circle(new Point(0, 0), 1);
+            Assert.That(c.Area,Is.EqualTo(Math.PI));
+        }
+        [Test]
+        public void CreationFromString()
+        {
+            var text = "";
+            Action<string> printer = (input) => text = input;
+            new Circle("1,1,1").Print(printer);
+            Assert.That(text, Is.EqualTo("Centre coordinates: (1,1), Radius: 1"));
+        }
+    }
+}
